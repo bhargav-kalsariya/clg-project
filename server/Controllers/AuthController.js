8127f7bc21003b40b28972d9254565bc29456499
bhargav-kalsariya <kalsariyabhargav187@gmail.com>
2024-03-09 15:50:31 +0530

    set the jwt verification in login functionality

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -1,4 +1,8 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config('../.env');
 
 const { User } = require("../Models/UserSchema");
 const { Failure, Success } = require("../utilities/ResponseWrapper");
@@ -21,13 +25,13 @@ const SignupHandler = async (req, res) => {
         const salt = await bcrypt.genSalt(saltRounds);
         const securedPassword = await bcrypt.hash(password, salt);
 
-        const user = await User.create({
+        await User.create({
             name,
             email,
             password: securedPassword
         })
 
-        return res.send(Success(201, { user }));
+        return res.send(Success(201, 'user created successfully'));
 
     }
 
@@ -45,25 +49,79 @@ const LoginHandler = async (req, res) => {
 
     }
 
-    const user = await User.findOne({ email: email }).select('password');
+    try {
 
-    if (!user) {
+        const user = await User.findOne({ email: email }).select('+password');
 
-        return res.send(Failure(404, 'user not found'));
+        if (!user) {
+
+            return res.send(Failure(404, 'user not found'));
+
+        }
+
+        const verifiedUser = await bcrypt.compare(password, user.password);
+
+        if (!verifiedUser) {
+
+            return res.send(Failure(403, 'Invalid password'));
+
+        }
+
+        const accessToken = generateAccessToken({ _id: user._id });
+        const refreshToken = generateRefreshToken({ _id: user._id });
+
+        res.cookie('jwt', refreshToken, {
+
+            httpOnly: true,
+            secure: true,
+
+        });
+
+        return res.send(Success(200, { accessToken }));
+
+    } catch (error) {
+
+        return res.send(Failure(500, { error }));
 
     }
-
-    const verifiedUser = await bcrypt.compare(password, user.password);
-
-    if (!verifiedUser) {
-
-        return res.send(Failure(403, 'Invalid password'));
-
-    }
-
-    return res.send(Success(200, 'User login successful'));
 
 }
+
+const generateAccessToken = (data) => {
+
+    try {
+
+        const accessToken = jwt.sign(data, process.env.ACCESSTOEKN_PRIVATE_KEY, {
+            expiresIn: '1d',
+        });
+
+        return accessToken;
+
+    } catch (error) {
+
+        return res.send(Failure(500, { error }));
+
+    }
+
+};
+
+const generateRefreshToken = (data) => {
+
+    try {
+
+        const refreshToken = jwt.sign(data, process.env.REFRESHTOKEN_PRIVATE_KEY, {
+            expiresIn: '1y',
+        });
+
+        return refreshToken;
+
+    } catch (error) {
+
+        return res.send(Failure(500, { error }));
+
+    }
+
+};
 
 module.exports = {
 
