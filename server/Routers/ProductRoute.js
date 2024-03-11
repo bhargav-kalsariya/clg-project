f39520903fedfad786fc831afff50e4305ded4f1
bhargav-kalsariya <kalsariyabhargav187@gmail.com>
2024-03-11 18:40:41 +0530

    allow customer to get all products

--- server/Routers/ProductRoute.js
+++ server/Routers/ProductRoute.js
@@ -3,7 +3,7 @@ const ProductController = require('../Controllers/ProductController');
 const AuthoraizedUser = require('../Middlewares/AuthoraizedUser');
 const isAdmin = require('../Middlewares/isAdmin');
 
-router.get('/', AuthoraizedUser, isAdmin, ProductController.getAllProductsHandler);
+router.get('/', AuthoraizedUser, ProductController.getAllProductsHandler);
 router.post('/create', AuthoraizedUser, isAdmin, ProductController.createProductHandler);
 
 module.exports = router;
