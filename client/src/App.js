0dfea8229285ce9e01d22496ff42e9d13710aea5
bhargav-kalsariya <kalsariyabhargav187@gmail.com>
2024-04-07 10:49:20 +0530

    created home page in fe and redirect user to it after login success

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Signup from './pages/signup/Signup';
 import Login from './pages/login/Login';
+import Home from './pages/home/Home';
 
 function App() {
     return (
         <div className="App">
             <Routes>
+                <Route path='/' element={<Home />}></Route>
                 <Route path='/signup' element={<Signup />}></Route>
                 <Route path='/login' element={<Login />}></Route>
             </Routes>
