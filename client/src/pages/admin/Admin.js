05b8abc8de6a7b2928ff662280d48ad49234e8c2
bhargav-kalsariya <kalsariyabhargav187@gmail.com>
2024-04-14 10:01:31 +0530

    removed some unwanted things from fe

--- client/src/pages/admin/Admin.js
+++ client/src/pages/admin/Admin.js
@@ -1,8 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { isAdmin } from '../../redux/Slices/userSlice';
-import { Navigate, Outlet } from 'react-router-dom';
-
 
 function Admin() {
 
