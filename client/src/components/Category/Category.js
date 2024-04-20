fb92e423d134dc91f5259f37f8ffebc0b5602ac5
bhargav-kalsariya <kalsariyabhargav187@gmail.com>
2024-04-20 19:14:10 +0530

    I fixed the image related problem in the category

--- client/src/components/Category/Category.js
+++ client/src/components/Category/Category.js
@@ -5,9 +5,9 @@ import { useNavigate } from 'react-router-dom';
 function Category({ category }) {
     const navigate = useNavigate();
     return (
-        <div className='Category' onClick={() => navigate(`category/${category._id}`)}>
+        <div className='Category' style={{ backgroundImage: `url(${category?.image?.url})` }} onClick={() => navigate(`category/${category?._id}`)}>
             <div className="category-content center">
-                <h3 className='heading'>{category.name}</h3>
+                <h3 className='heading'>{category?.name}</h3>
             </div>
         </div>
     )
