6ae1b08718b0bc05ed9514675809cac46e5ab0b8
bhargav-kalsariya <kalsariyabhargav187@gmail.com>
2024-04-25 11:48:46 +0530

    designed delete product component in fe

--- client/src/components/DeleteProduct/DeleteProduct.js
+++ client/src/components/DeleteProduct/DeleteProduct.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './DeleteProduct.scss'
+import { axiosClient } from '../../utilities/axiosClient';
 
 function DeleteProduct() {
+
+    const [products, setProducts] = useState([]);
+    const [productId, setProductId] = useState('');
+
+    async function fetchData() {
+        const product = await axiosClient.get(`/product/all`);
+        setProducts(product.data.result.products);
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    async function handleDelete() { }
+
     return (
-        <div>DeleteProduct</div>
+        <div className='ProductList'>
+
+            {products.map((product) =>
+                <div className='ProductItem'>
+                    <div className="product-container">
+                        <div className="product-img">
+                            <div className="img-container">
+                                <img src={product?.image?.url} alt={product?.title} className='product-image' />
+                            </div>
+                        </div>
+                        <div className="product-info">
+                            <div className="product-title">{product?.title}</div>
+                            <div className="product-price">
+                                ₹ {product?.price}</div>
+                            <button className="delete-button" onClick={handleDelete}>Delete</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+        </div>
+
     )
 }
 
